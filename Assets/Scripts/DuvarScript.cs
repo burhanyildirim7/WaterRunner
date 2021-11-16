548f7b46c8c76de2f3896f7ed3e350c50a4226d1
KuntayK <kuntay.koca.development@gmail.com>
2021-11-17 00:41:00 +0300

    level 1 tasarlandı

--- Assets/Scripts/DuvarScript.cs
+++ Assets/Scripts/DuvarScript.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DuvarScript : MonoBehaviour
 {
-
+    [SerializeField] Text TemizlenmeMiktariText;
+    [SerializeField] Text KirlenmeMiktariText;
+    [SerializeField] bool temizleme;
+    [SerializeField] bool kirletme;
     public int _duvarDegeri;
 
 }
